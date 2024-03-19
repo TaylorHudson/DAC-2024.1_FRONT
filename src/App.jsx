f38610f1ece0a2074b9d6b1ff978c92a5aad4a6a
TaylorHudson <taylorhudsonsantos@gmail.com>
2024-03-19 16:59:41 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import CreateProductView from "./views/CreateProductView";
+import CreateCategoryView from "./views/CreateCategoryView";
+
+function App() {
+  return (
+    <div className="App">
+      <CreateCategoryView />   
+    </div>
+  );
+}
+
+export default App;
